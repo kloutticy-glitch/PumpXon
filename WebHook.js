ef4a8475595ece21336dd87976df9101441c0f74
kloutticy-glitch <kloutticy@gmail.com>
2026-06-23 03:20:26 -0500

    Add webhook handler for Telegram messages

--- WebHook.js
+++ WebHook.js
@@ -1,1 +1,11 @@
 
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    // This is where Telegram sends the messages
+    console.log("Message received:", req.body);
+    res.status(200).json({ status: 'ok' });
+  } else {
+    // This just shows a message if someone visits the link in a browser
+    res.status(200).send('Webhook is active!');
+  }
+}
